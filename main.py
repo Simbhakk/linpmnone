d4ced3e4d1a4fdd043c86008849cb2f5bb424c77
Simbhakk <127213964+Simbhakk@users.noreply.github.com>
2024-06-15 13:19:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -162,7 +162,7 @@ def save(client: pyrogram.client.Client, message: pyrogram.types.messages_and_me
         except:
             toID = fromID
 
-        for msgid in range(fromID, toID+1):
+        for msgid in range(fromID, fromId+1):
             username = datas[3]
             try:
                 msg = bot.get_messages(username, msgid)
